72ab8839d529beec9fb79687aefb14ff2e0418c6
sriloshik <83942997+sriloshik@users.noreply.github.com>
2021-05-20 14:38:31 +0530

    Add files via upload

---
+++ MultiThreading/Synchronization/PrinterThread.java
@@ -0,0 +1,13 @@
+public class PrinterThread extends Thread{
+	String s1;
+	String s2;
+	PrinterThread(String s1,String s2)
+	{
+		this.s1=s1;
+		this.s2=s2;
+	}
+public synchronized void  run()
+{
+	System.out.println(s1+s2);
+}
+}
